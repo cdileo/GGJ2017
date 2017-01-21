613b29375dde20d3f721e4b0c4d88430e3fc1bc9
Chris Dileo <chris.dileo@gmail.com>
2017-01-21 12:03:12 -0800

    background is in, just off centre

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'mainDiv', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'mainDiv', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.image('star', 'assets/star.png');
     game.load.image('lazybound', 'assets/lazybound.png');
     game.load.script('input', 'js/input.js');
-    game.load.image('backgroundWaves', 'assets/tempBackground.png');
+    game.load.image('waves', 'assets/tempBackground.png');
     game.load.physics('waveCollider', 'assets/tempBackgroundCollider.json');
 }
 
 var ggj = {};
 
 function create() {
-    ggj.player = game.add.sprite(32, game.world.height - 150, 'star');
-    game.physics.arcade.enable(ggj.player);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    ggj.player = game.add.sprite(0, 0, 'star');
+    game.physics.p2.enable(ggj.player);
     ggj.player.body.collideWorldBounds = true;
 
-    ggj.horizon = game.add.sprite(0, game.world.height/2, 'lazybound');
-    ggj.horizon.scale.setTo(1, ggj.horizon.scaleMax);
+    ggj.horizon = game.add.sprite(0, game.world.height, 'waves');
+    game.physics.p2.enable([ggj.horizon], true);
+    ggj.horizon.body.clearShapes();
+    ggj.horizon.body.loadPolygon('waveCollider');
 
     //  Our controls.
     ggj.keyboard = game.input.keyboard.createCursorKeys();

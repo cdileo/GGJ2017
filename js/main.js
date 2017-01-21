087156fb61bc259068a51cb66837862de6c615f0
Chris Dileo <chris.dileo@gmail.com>
2017-01-21 14:53:09 -0800

    Moving the wave sprite around to try to find its collider

--- js/main.js
+++ js/main.js
@@ -14,25 +14,47 @@ var ggj = {};
 
 function create() {
     game.physics.startSystem(Phaser.Physics.P2JS);
-    ggj.player = game.add.sprite(32, game.world.height - 150, 'star');
+
+    // Waves
+    ggj.horizon = game.add.sprite(game.world.width + 50, game.world.height + 50, 'waves');
+    hor = ggj.horizon;
+    game.physics.p2.enable([hor], true);
+    hor.body.clearShapes();
+    if (hor.body.loadPolygon('waveCollider')) console.log('Succeded in loading new wave poly');
+    // Change our polygon to be only a sensor, not a physics collider.
+    // for (let i in hor.body.data.shapes) {
+    //     hor.body.data.shapes[i].sensor = true;
+    // }
+    hor.anchor.x = 1;
+    hor.anchor.y = 1;
+    hor.body.onBeginContact.add(displayOverlapState, this);
+
+    // Player
+    ggj.player = game.add.sprite(50, 50, 'star');
     game.physics.p2.enable(ggj.player, true);
     ggj.player.body.collideWorldBounds = true;
     ggj.player.body.mass = .1;
-
-    ggj.horizon = game.add.sprite(0, game.world.height, 'waves');
-    game.physics.p2.enable([ggj.horizon], true);
-    ggj.horizon.body.clearShapes();
-    ggj.horizon.body.loadPolygon('waveCollider');
 
     //  Our controls.
     ggj.keyboard = game.input.keyboard.createCursorKeys();
 
     ggj.scoreText = game.add.text(16, 16, 'Hey', { fontSize: '32px', fill: '#fff' });
+    ggj.collisionText = game.add.text(16, 32, 'Hey', { fontSize: '32px', fill: '#fff' });
 }
 
 function update() {
     moveThing(ggj.player, ggj.keyboard, ggj.horizon);
     displaySpeeds(ggj.player);
+}
+
+// Simple test for overlap state
+function displayOverlapState(e) {
+    // obj1.body.onBeginContact.add(handleOverlapListener, this);
+    console.log(e);
+}
+
+function handleOverlapListener() {
+    console.log('Overlap!');
 }
 
 function displaySpeeds(player) {
